7aadc86381a96ecb9716a17ba4b4e1ba506d974c
Abecchy <chuke_k.1998@icloud.com>
2024-02-29 00:02:21 +0900

    事故画一覧ページ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Userモデル5件
+5.times do |n|
+  User.create!(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
+# Postモデル20件
+20.times do |n|
+  user = User.offset( rand(User.count) ).first
+  Post.create!(
+    title: Faker::Games::Pokemon.unique.name,
+    body: "#{Faker::Games::Pokemon.move} を繰り出した！",
+    user_id: user.id
+  )
+end
