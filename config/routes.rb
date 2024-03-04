18616122fdc4f9c4b8e4ca7b801da66e04125480
Abecchy <chuke_k.1998@icloud.com>
2024-03-04 14:14:55 +0900

    プロフィールページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get "likes", on: :collection
   end
   resources :likes, only: %i[create destroy]
+  resource :profile, only: %i[show edit update]
 
   get "login", to: "user_sessions#new"
 	post "login", to: "user_sessions#create"
