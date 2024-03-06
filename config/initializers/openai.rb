290bdb81a2aeb7f4317cacf96b993ad99e502866
Abecchy <chuke_k.1998@icloud.com>
2024-03-06 12:34:11 +0900

    AIタイトル生成機能の修正

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,9 @@
-OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.chatgpt_api_key
+if Rails.env.production?
+  OpenAI.configure do |config|
+    config.access_token = ENV['OPENAI_API_KEY']
+  end
+else
+  OpenAI.configure do |config|
+    config.access_token = Rails.application.credentials.chatgpt_api_key
+  end
 end
