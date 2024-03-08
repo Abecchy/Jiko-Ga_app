2f8601f67d0761580d89566a462cbfe0bca24ff1
Abecchy <chuke_k.1998@icloud.com>
2024-03-08 15:45:42 +0900

    会員登録バリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :pets, allow_destroy: true, reject_if: :all_blank
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, uniqueness: { case_sensitive: false }, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, uniqueness: true, allow_nil: true
