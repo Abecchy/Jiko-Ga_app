6109186f795ef1e91b362dbaa42ebd990b07673b
Abecchy <chuke_k.1998@icloud.com>
2024-03-18 22:34:22 +0900

    フォローボタンの非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,21 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to posts_path, success: t('.success')
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+
+    respond_to do |format|
+      format.html { redirect_to users_path, success: t('.success') }
+      format.turbo_stream
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to posts_path, success: t('.success'), status: :see_other
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+
+    respond_to do |format|
+      format.html { redirect_to users_path, success: t('.success'), status: :see_other }
+      format.turbo_stream
+    end
   end
 end
