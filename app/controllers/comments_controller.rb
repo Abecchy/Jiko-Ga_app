3f30030afdce9ad96ed10a597a4ef73afdc7aef6
Abecchy <chuke_k.1998@icloud.com>
2024-04-18 21:28:24 +0900

    非同期なコメント編集機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,36 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.save
+    flash.now[:success] = t('defaults.flash_message.created', item: Comment.model_name.human)
+  end
+
+  def edit
+    @comment = current_user.comments.find(params[:id])
+    @post = @comment.post
+  end
+
+  def update
+    @comment = current_user.comments.find(params[:id])
+    if @comment.update(comment_update_params)
+      flash.now[:success] = t('defaults.flash_message.updated', item: Comment.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: Comment.model_name.human)
+    end
   end
 
   def destroy
     @comment = current_user.comments.find(params[:id])
     @comment.destroy!
+    flash.now[:success] = t('defaults.flash_message.deleted', item: Comment.model_name.human)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:body).merge(post_id: params[:post_id])
+  end
+
+  def comment_update_params
+    params.require(:comment).permit(:body).merge(post_id: @comment.post_id)
   end
 end
